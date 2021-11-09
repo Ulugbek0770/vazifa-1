7711c8c17999fd31cac8009194e3117036432fb2
ulugbek <ulugbek>
2021-11-09 20:22:17 +0500

    vazifa

---
+++ script.js
@@ -0,0 +1,108 @@
+var name = prompt("Ismingizni kriting")
+console.log("Sizning ismingiz " + name);
+
+var age = +prompt("Yoshingizni kriting")
+console.log("Sizning "+ age + " da")
+
+alert("Misolarni ishlang")
+
+b = 29
+
+var num = +prompt("5 + 24 = ? ")
+if (num == b ){
+    console.log("Siznin javob to`gri " + num);
+    }
+else {
+    console.log("Siznin javobingiz notog`ri " + num + " to`gri javob 29");}
+
+a = 47
+var num = +prompt("34 + 13 = ?")
+if( num == a){
+    console.log("Sizning javobingiz to`g`ri " + num);
+    }
+else{
+    console.log("Siznin javobingiz noto`g`ri " + num + " to`g`ri javob " + a);
+}
+
+c= 81
+
+var num = +prompt("9 * 9 = ?")
+ if (num == c) {
+     console.log("Sizning javobimgiz to`g`ri " + num);
+
+ }
+else{
+    console.log("Sizning javoingiz noto`g`ri " + num + " to`g`ri javob " + c );
+}
+
+
+d = 4
+
+var num = +prompt(" 200 / 50 = ?")
+if( num == d){
+    console.log("Sizning javobingiz to`g`ri " + num);
+}
+else{
+    console.log("Sizning javobingiz noto`g`ri " + num + " to`g`ri javob " + d);
+}
+
+
+e = 1
+
+var num = +prompt("25 % 4 = ?")
+if( num == e){
+    console.log("Sizning javobingiz to`g`ri" + num);
+}
+else{
+    console.log("Sizning javobingiz noto`gri " + num + " to`g`ri javob " + e );
+}
+
+
+
+r = 25 
+var num = +prompt("75 - 50 = ?")
+if( num == r){
+    console.log("Sizning javobingiz to`g`ri " + num);
+
+}
+else{
+    console.log("Sizning javobingiz noto`gri " + num + " to`g`ri javob " + r);
+}
+
+
+t = 49
+var num = +prompt("7 * 7 = ?")
+if(num == t){
+    console.log("Sizning javobingiz to`g`ri " + num);
+}
+
+else{
+    console.log("Sizning javobingiz noto`gri " + num + " to`g`ri javob " + t);
+}
+
+y= 300
+
+var num = +prompt("25 * 12 = ?")
+if ( num == y) {
+    console.log("Sizning javobingiz to`g`ri " + num);
+}
+else{
+    console.log("Sizning javobingiz noto`gri " + num + " to`g`ri javob " + y);
+}
+
+u = 3
+var num = +prompt("99 % 8 = ? ")
+if (num == u) {
+    console.log("Sizning javobingiz to`g`ri" + num);
+}
+else{
+    console.log("Sizning javobingiz noto`gri " + num + " to`g`ri javob " + u);
+}
+
+i = 45
+var num = +prompt("155 - 110 = ?")
+if (num == i){
+    console.log("Sizning javobingiz to`g`ri " + num);
+}
+else{
+    console.log("Sizning javobingiz noto`gri " + num + " to`g`ri javob " + i);}
